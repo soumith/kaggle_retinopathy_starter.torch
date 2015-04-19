9a69125d23c6adcb23e5212fe8f842ca0ec747d0
Akash Mehra <mehra.akash@gmail.com>
2015-04-19 17:56:13 -0400

    Added a model file based on all cnn paper. Need to test it by running the network.

---
+++ models/allcnn.lua
@@ -0,0 +1,31 @@
+function createModel(nGPU)
+   require 'cudnn'
+
+   convnet = nn.Sequential()
+   convnet:add(nn.SpatialConvolution(1,96,3,3))
+   convnet:add(nn.ReLU())
+   convnet:add(nn.SpatialConvolution(96,96,3,3))
+   convnet:add(nn.ReLU())
+   convnet:add(nn.SpatialConvolution(96,192,3,3,2,2))
+   --convnet:add(nn.ReLU())
+   convnet:add(nn.SpatialConvolution(192,192,3,3))
+   --convnet:add(nn.ReLU())
+   convnet:add(nn.SpatialConvolution(192,192,3,3))
+   --convnet:add(nn.ReLU())
+   convnet:add(nn.SpatialConvolution(192,192,3,3,2,2))
+   --convnet:add(nn.ReLU())
+   convnet:add(nn.SpatialMaxPooling(3,3,2,2))
+   convnet:add(nn.SpatialConvolution(192,192,3,3))
+   --convnet:add(nn.ReLU())
+   convnet:add(nn.SpatialConvolution(192,192,1,1))
+   --convnet:add(nn.ReLU())
+   convnet:add(nn.SpatialConvolution(192,10,1,1))
+   --convnet:add(nn.ReLU())
+   convnet:add(nn.View(10*27*27))
+   convnet:add(nn.Linear(10*27*27,10))
+   convnet:add(nn.Linear(10,nClasses))
+   convnet:add(nn.LogSoftMax())
+
+   return convnet 
+end
+
